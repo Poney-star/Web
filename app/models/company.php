9e1334c5a2bc93ebe0777ac459c7868ac27ab7b0
isaac <maarekisaac@gmail.com>
2025-04-01 19:46:28 +0200

    restructuration + ajouts de pages, ajout de la connexion à la base de donnée en tant que controlleur.

--- app/models/company.php
+++ app/models/company.php
@@ -1,5 +1,5 @@
 <?php
-class Company {
+class CompanyModel {
 
     private $pdo;
 
--- public/index.php
+++ public/index.php
@@ -2,14 +2,30 @@
 
 require_once '../vendor/autoload.php';
 
-require_once '../app/controllers/HomepageController';
+require_once '../app/controllers/HomeController';
+require_once '../app/controllers/ContactController';
+require_once '../app/controllers/CompaniesController';
+require_once '../app/controllers/OffersController';
+require_once '../app/controllers/ErrorController';
 
-// Simulation d'une route
-$page = $_GET['page'] ?? 'homepage';
+$page = $_GET['page'] ?? 'home';
 
-if ($page === 'homepage') {
+if ($page === 'home') 
+{
     $controller = new HomepageController();
-    $controller->loadHomepage();
+} else if ($page === 'contact')
+{
+    $controller = new ContactpageController();
+} else if ($page === 'companies')
+{
+    $index = $_GET['i'] ?? '1';
+    $controller = new CompaniespageController($index);
+} else if ($page === 'offers')
+{
+    $index = $_GET['i'] ?? '1';
+    $controller = new OfferspageController($index);
 } else {
-    echo "Page non trouvée";
+    $controller = new ErrorpageController();
 }
+
+$controller->loadpage();
